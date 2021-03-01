b2f537a8e94c496a88715da7e71e4b92052a17cd
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-03-01 15:10:51 +0100

    perfectionnalisation

--- lib/screen/admin/new_programme.dart
+++ lib/screen/admin/new_programme.dart
@@ -89,7 +89,7 @@ class _NewProgrammeState extends State<NewProgramme> {
                                         lastDate: DateTime(2021, 10));
                                   },
                                   child: Container(
-                                    child: Text('tooks'),
+                                    child: Text('took'),
                                   ),
                                 ),
                                 Container(
